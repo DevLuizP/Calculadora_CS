0a3d8236ed081f8b28ab0381a86126094a3b26b3
Luiz Paulo Cardoso <70473328+DevLuizP@users.noreply.github.com>
2022-08-11 10:39:45 -0300

    Update Program.cs

--- Calculador/Program.cs
+++ Calculador/Program.cs
@@ -1,6 +1,56 @@
-﻿int a = 38;
-int b = 149;
-int c = a + b;
+﻿int num1 = 0; int num2 = 0; string loop = "";
 
-Console.WriteLine($"O resultado de {a} + {b} é igual a {c}");
-Console.ReadKey(true);
+static string answerToUpper()
+{
+    string loop = Console.ReadLine();
+    loop = loop.ToUpper();
+    return loop;
+}
+
+
+
+Console.WriteLine("Calculadora em console C#\r");
+Console.WriteLine("-------------------------\n");
+do
+{
+    Console.WriteLine("Digite um número, e tecle Enter");
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo número, e tecle Enter");
+    num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Escolha uma opção na lista abaixo:");
+    Console.WriteLine("\t1 - Adição");
+    Console.WriteLine("\t2 - Subtração");
+    Console.WriteLine("\t3 - Multiplicação");
+    Console.WriteLine("\t4 - Divisão");
+
+    var option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            Console.WriteLine($"Resultado: {num1} + {num2} = {num1 + num2}");
+            break;
+
+        case "2":
+            Console.WriteLine($"Resultado: {num1} - {num2} = {num1 - num2}");
+            break;
+
+        case "3":
+            Console.WriteLine($"Resultado: {num1} x {num2} = {num1 * num2}");
+            break;
+
+        case "4":
+            Console.WriteLine($"Resultado: {num1} / {num2} = {num1 / num2}");
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    };
+    Console.WriteLine("Deseja realizar outra operação?");
+    Console.WriteLine("\nS - Sim");
+    Console.WriteLine("\nN - Não");
+    loop = answerToUpper();
+} while (loop == "S");
+
